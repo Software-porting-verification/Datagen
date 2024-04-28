27c569ee91bbc7569b2e224d051b21633dd01d82
maoif <maoif@foxmail.com>
2024-04-28 09:55:08 +0800

    Add perf-wrapper.py and datagen.py.

---
+++ datagen.py
@@ -0,0 +1,117 @@
+#! /usr/bin/env python3
+
+####################################################
+#
+#
+# dataset generator
+#
+# Author: Mao Yifu, maoif@ios.ac.cn
+#
+#
+####################################################
+
+import sys
+import argparse
+import yaml
+import trace_datum
+from trace_datum import *
+
+
+g_package: str = ''
+g_version: str = ''
+
+def analyze_args():
+    pass
+
+
+def analyze_envs():
+    pass
+
+
+def filter_result(datum: TraceDatum):
+    # TODO filter .sh?
+
+    filter_prefixes = ['/bin/', '/usr/', '/sbin/']
+    callee = datum.file_path
+    for p in filter_prefixes:
+       if callee.startswith(p):
+            return False
+
+    filter_suffixes = ['./conftest', './configure', '.build.command', '/bin/sh', '/.', '.sh']
+    for p in filter_suffixes:
+       if callee.endswith(p):
+            return False
+
+    filter_infixes = ['./exec.cmd']
+    for p in filter_infixes:
+       if p in callee:
+            return False
+
+    return datum.check_fields()
+
+
+def analyze_for_perf(data: list[TraceDatum]):
+    # TODO need manual filtering of scripts/binaries? Maybe do this later
+    binaries = []
+
+    for d in data:
+        exe = d.file_path
+        # caller's working dir
+        pwd = d.working_dir
+
+        # convert to absolute path
+        if not exe.startswith('/'):
+            exe = pwd + '/' + exe
+
+        binaries.append(exe)
+    
+    # deduplicate
+    return list(set(binaries))
+
+
+def analyzer_for_fuzz(data: list[TraceDatum]):
+    return []
+
+
+def analyze(data: list[TraceDatum]):
+    filtered = list(filter(lambda d: filter_result(d), data))
+    fuzz = analyzer_for_fuzz(filtered)
+    perf = analyze_for_perf(filtered)
+
+    return fuzz, perf
+
+
+
+###
+### start of program
+###
+
+parser = argparse.ArgumentParser(
+    prog='datagen',
+    description='Analyze bpftrace data and generate dataset.')
+parser.add_argument('-f', '--files', nargs='+', required=True, help='trace files in yaml format')
+
+args = parser.parse_args()
+
+trace_files = args.files
+for tf in trace_files:
+    with open(tf, 'r') as f:
+        print(f'loading {tf}')
+        data = yaml.load(f, Loader=yaml.Loader)
+        print(f'loading {tf} done')
+        g_package = data['package']
+        g_version = data['version']
+        fuzz, perf = analyze(data['data'])
+        # TODO format of fuzz and perf dataset
+        
+        fuzz_path = f'{g_package}-{g_version}-fuzz'
+        perf_path = f'{g_package}-{g_version}-perf'
+        # with open(fuzz_path, 'w') as f_fuzz:
+            # print(f'{tf} fuzz dataset at {fuzz_path}')
+            # pass
+        with open(perf_path, 'w') as f_perf:
+            yaml.dump(perf, f_perf)
+            print(f'{tf} perf dataset at {perf_path}')
+
+            
+        # print(f"{len(data['data'])}")
---
+++ perf-wrapper.py
@@ -0,0 +1,148 @@
+#! /usr/bin/env python3
+
+####################################################
+#
+#
+# perf command wrapper
+#
+# Author: Mao Yifu, maoif@ios.ac.cn
+#
+#
+####################################################
+
+import shutil
+import os
+import subprocess
+import yaml
+import argparse
+
+g_package: str = ''
+g_version: str = ''
+g_dataset_path: str = ''
+g_perf_data_path: str = ''
+g_perf_dir_env: str = 'TREC_PERF_DIR'
+g_perf_dry_run_env: str = 'PERF_WRAPPER_DRY_RUN'
+g_dry_run: bool = False
+
+
+def notice(msg: str = ''):
+    if g_dry_run:
+        print(f'[perf-wrapper dry run] {msg}')
+    else:
+        print(f'[perf-wrapper] {msg}')
+
+
+def wrap(exe: str):
+    # check existence, is binary, etc.
+    if not os.path.exists(exe):
+        notice(f'{exe} not found, skipping')
+        return
+    elif not os.path.isfile(exe):
+        return
+    else:
+        # check 'ASCII text' or 'ELF'
+        res = subprocess.run(["file", exe], capture_output=True, text=True)
+        res.check_returncode()
+        if 'ASCII text' in res.stdout:
+            # skip non-binaries
+            notice(f'{exe} is not binary, contents:')
+            subprocess.run(["cat", exe])
+            notice()
+            return
+    
+    base_exe = os.path.basename(exe)
+    backup = f'{exe}.perf-bin'
+    out = f'{base_exe}.perf.data'
+
+    if g_dry_run:
+        notice(f'mv {exe} {backup}')
+    else:
+        notice(f'mv {exe} {backup}')
+        shutil.move(exe, backup)
+    
+    # TODO parameterize more? e.g., by reading envs
+    exe_script=f"""
+#!/usr/bin/env bash
+
+SUFIX=$(date +%N_%F_%T)
+perf record -F 999 -ag -o {g_perf_data_path}/{out}.$SUFIX {backup} $@
+"""
+
+    if g_dry_run:
+        notice(f'writing {exe} as\n{exe_script}')
+        notice()
+    else:
+        with open(exe, 'w') as f:
+            f.write(exe_script)
+            notice(f'writing {exe} as\n{exe_script}')
+            notice()
+
+        res = subprocess.run(["chmod", "+x", exe], capture_output=True, text=True)
+        res.check_returncode()
+        notice(f'written wrapper {exe}')
+
+
+###
+### start of program
+###
+
+parser = argparse.ArgumentParser(
+    prog='perf-wrapper',
+    description='Replace executables with perf testing scripts.')
+parser.add_argument('-p', '--package', required=True, help='package name')
+parser.add_argument('-v', '--version', required=True, help='package version')
+parser.add_argument('-d', '--dry', action='store_true', help='dry run, without real replacing')
+
+args = parser.parse_args()
+
+g_package  = args.package
+g_version  = args.version
+g_dataset_path = f"/usr/lib64/rvbench/{g_package}-{g_version}-perf"
+g_dry_run = args.dry
+
+if g_perf_dry_run_env in os.environ.keys():
+    v = os.environ[g_perf_dry_run_env]
+    if v == 'ON':
+        g_dry_run = True
+    elif v == 'OFF':
+        g_dry_run = False
+    else:
+        notice(f'Bad {g_perf_dry_run_env} value, default to OFF')
+
+if g_dry_run:
+    notice('running in dry run mode')
+
+# look for dataset in appropriate location
+# search for each executable in the dataset
+# rename and replace
+
+# 1. rename: mv exe exe.perf-bin
+# 2. wrap: 
+# exe.perf-bin -> 
+# #!/bin/env bash
+# perf record ... exe.perf-bin -o perf.data
+
+# TODO check package name and version
+
+if not os.path.exists(g_dataset_path):
+    notice(f'Dataset {g_dataset_path} not found, quitting...')
+    exit(-1)
+elif os.path.isdir(g_dataset_path):
+    notice(f'Dataset {g_dataset_path} is not a file, quiting...')
+    exit(-1)
+
+if not g_perf_dir_env in os.environ.keys():
+    notice('env TREC_PERF_DIR not set, quitting...')
+    exit(-1)
+
+g_perf_data_path = os.environ[g_perf_dir_env]
+if not os.path.isdir(g_perf_data_path):
+    notice(f'{g_perf_data_path} is not a dir, quiting...')
+    exit(-1)
+
+notice(f'perf data will be stored in {g_perf_data_path}')
+
+with open(g_dataset_path, 'r') as f:
+    exes = yaml.load(f, Loader=yaml.Loader)
+    for exe in exes:
+        wrap(exe)
